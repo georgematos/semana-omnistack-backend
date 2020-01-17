b56d290e1cd75b2bca94144a5687d89a0747655e
George Matos <georgemattos@gmail.com>
2020-01-17 15:46:52 -0300

    WebSocket - atualizando mapa com novos devs em tempo real

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://georgematos:oOvGkpxLYNuqy3SZ@cluster0-tftmc.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +20,4 @@ app.use(cors());
 app.use(express.json()); // Express passará a entender requisições enviadas como json
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
